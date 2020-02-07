90abf72833c0b6d822cc5ae8945acc4762479709
Sergiu Mosanu <sm7ed@virginia.edu>
2020-02-07 10:53:32 -0500

    add AWSF1 legacy files

---
+++ AWSF1_SDx_BWA_SW/src/host.cpp
@@ -0,0 +1,258 @@
+#include "ksw_ext2.h"
+#include <stdio.h>
+#include <ap_int.h>
+
+#include <time.h>
+#include <chrono>
+
+extern "C" {
+#include "bwa.h"
+#include "bwa.c"
+}
+
+static const int qr_max = 256;
+static const int db_max = 256;
+static const int r_vars = 6;
+static const int m = 5;
+static const int smlen = m * m;
+
+int getRandomNumber(int min, int max) {
+	return (rand() % (max - min + 1)) + min;
+}
+
+int getRandomCharacter() {
+	return rand() % m;
+}
+
+struct ResultC {
+	int max;
+	int max_i;
+	int max_j;
+	int max_ie;
+	int gscore;
+	int max_off;
+};
+
+int main(int argc, char* argv[]) {
+
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " <xclbin>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	char* xclbinFilename = argv[1];
+
+	// Creates the vectors to be sent / returned
+	vector<ap_uint<4>, aligned_allocator<ap_uint<4>>> source_q(qr_max);
+	vector<ap_uint<4>, aligned_allocator<ap_uint<4>>> source_t(db_max);
+	vector<short, aligned_allocator<short>> source_m(r_vars);
+
+	// Compute the size of array in bytes
+	size_t size_in_bytes_q = qr_max * sizeof(ap_uint<4> );
+	size_t size_in_bytes_t = db_max * sizeof(ap_uint<4> );
+	size_t size_in_bytes_m = r_vars * sizeof(short);
+
+	// Getting First Platform
+	std::vector<cl::Platform> platforms;
+	cl::Platform::get(&platforms);
+	cl::Platform platform = platforms[0];
+	std::cout << "Platform: " << platform.getInfo<CL_PLATFORM_NAME>() << "\n";
+
+	// Getting ACCELERATOR Devices and selecting 1st such device
+	std::vector<cl::Device> devices;
+	platform.getDevices(CL_DEVICE_TYPE_ACCELERATOR, &devices);
+	cl::Device device = devices[0];
+	std::cout << "Device: " << device.getInfo<CL_DEVICE_NAME>() << "\n";
+
+	// Creating Context and Command Queue for selected device
+	cl::Context context(device);
+	cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE);
+
+	// Load
+	std::cout << "Loading: '" << xclbinFilename << "'\n\n";
+	std::ifstream bin_file(xclbinFilename, std::ifstream::binary);
+	bin_file.seekg(0, bin_file.end);
+	unsigned nb = bin_file.tellg();
+	bin_file.seekg(0, bin_file.beg);
+	char *buf = new char[nb];
+	bin_file.read(buf, nb);
+
+	// These commands will allocate memory on the FPGA. The cl::Buffer objects can
+	// be used to reference the memory locations on the device. The cl::Buffer
+	// object cannot be referenced directly and must be passed to other OpenCL
+	// functions.
+	cl::Buffer buffer_q(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+			size_in_bytes_q, source_q.data());
+	cl::Buffer buffer_t(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+			size_in_bytes_t, source_t.data());
+	cl::Buffer buffer_m(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+			size_in_bytes_m, source_m.data());
+
+	//Separate Read/write Buffer vector is needed to migrate data between host/device
+	std::vector<cl::Memory> inBufVec, outBufVec;
+	inBufVec.push_back(buffer_q);
+	inBufVec.push_back(buffer_t);
+	outBufVec.push_back(buffer_m);
+
+	// Creating Program from Binary File
+	cl::Program::Binaries bins;
+	bins.push_back( { buf, nb });
+	devices.resize(1);
+	cl::Program program(context, devices, bins);
+
+	// This call will extract a kernel out of the program we loaded in the
+	// previous line. A kernel is an OpenCL function that is executed on the
+	// FPGA. This function is defined in the src/ksw_ext2.cl file.
+	cl::Kernel krnl_ksw_ext2(program, "ksw_ext2");
+
+	bool all_tests_passed = true;
+	int test_j = 0;
+	for (test_j = 1; test_j <= 333; test_j++) {
+		srand(test_j);
+		int o_del = getRandomNumber(2, 5);
+		int e_del = getRandomNumber(2, 5);
+		int o_ins = getRandomNumber(2, 5);
+		int e_ins = getRandomNumber(2, 5);
+		int match = getRandomNumber(2, 5);
+		int mismatch = getRandomNumber(1, 3); // will use the negative of this
+		int ambiguous = getRandomNumber(1, 2); // will use the negative of this
+		int end_bonus = getRandomNumber(3, 7);
+		int qr_len = getRandomNumber(8, qr_max); // query length
+		int db_len = getRandomNumber(qr_max, db_max); // target length
+
+		int w = getRandomNumber(1, 100);
+		int h0 = getRandomNumber(qr_max / 2, qr_max);
+
+		int db_qr_element;
+
+		// generate db
+		uint8_t* db_c = new uint8_t[db_max];
+		for (int i = 0; i < db_max; i++) {
+			if (i < db_len) {
+				db_qr_element = getRandomCharacter();
+			} else {
+				db_qr_element = 0;
+			}
+			source_t[i] = (ap_uint<4> ) db_qr_element;
+			db_c[i] = (uint8_t) db_qr_element;
+		}
+
+		// generate qr
+		uint8_t* qr_c = new uint8_t[qr_max];
+		for (int i = 0; i < qr_max; i++) {
+			if (i < qr_len) {
+				db_qr_element = getRandomCharacter();
+			} else {
+				db_qr_element = 0;
+			}
+			source_q[i] = (ap_uint<4> ) db_qr_element;
+			qr_c[i] = (uint8_t) db_qr_element;
+		}
+
+		//declare the similarity matrix
+		int8_t* simat = new int8_t[smlen]; // similarity matrix pointer
+		int ij = 0;
+		for (int i = 0; i < m - 1; ++i) {
+			for (int j = 0; j < m - 1; ++j) {
+				simat[ij++] = i == j ? match : -mismatch;
+			}
+			simat[ij++] = -ambiguous; // ambiguous base
+		}
+		for (int j = 0; j < m; ++j) {
+			simat[ij++] = -ambiguous;
+		}
+
+		// declare function parameters and execute first part of the algorithm on the CPU
+		/**************************************************/
+		// adjust $w if it is too large
+		int i, max, max_ins, max_del, k = m * m, wh = w;
+		for (i = 0, max = 0; i < k; ++i) // get the max score
+			max = max > simat[i] ? max : simat[i];
+		max_ins = (int) ((double) (qr_len * max + end_bonus - o_ins) / e_ins
+				+ 1.);
+		max_ins = max_ins > 1 ? max_ins : 1;
+		wh = wh < max_ins ? wh : max_ins;
+		max_del = (int) ((double) (qr_len * max + end_bonus - o_del) / e_del
+				+ 1.);
+		max_del = max_del > 1 ? max_del : 1;
+		wh = wh < max_del ? wh : max_del; // TODO: is this necessary?
+		/**************************************************/
+
+		printf(
+				"test %d: db_len=%d qr_len=%d w=%d match=%d mismatch=%d ambiguous=%d o_del=%d e_del=%d o_ins=%d e_ins=%d end_bonus=%d\n",
+				test_j, db_len, qr_len, w, match, -mismatch, -ambiguous, o_del,
+				e_del, o_ins, e_ins, end_bonus);
+
+		// These commands will load the source_a and source_b vectors from the host
+		// application and into the buffer_a and buffer_b cl::Buffer objects. The data
+		// will be be transferred from system memory over PCIe to the FPGA on-board
+		// DDR memory.
+		q.enqueueMigrateMemObjects(inBufVec, 0/* 0 means from host*/);
+
+		//set the kernel Arguments
+		int narg = 0;
+		krnl_ksw_ext2.setArg(narg++, (ushort) qr_len);
+		krnl_ksw_ext2.setArg(narg++, buffer_q);
+		krnl_ksw_ext2.setArg(narg++, (ushort) db_len);
+		krnl_ksw_ext2.setArg(narg++, buffer_t);
+		krnl_ksw_ext2.setArg(narg++, (ushort) match);
+		krnl_ksw_ext2.setArg(narg++, (ushort) mismatch);
+		krnl_ksw_ext2.setArg(narg++, (ushort) ambiguous);
+		krnl_ksw_ext2.setArg(narg++, (ushort) o_del);
+		krnl_ksw_ext2.setArg(narg++, (ushort) e_del);
+		krnl_ksw_ext2.setArg(narg++, (ushort) o_ins);
+		krnl_ksw_ext2.setArg(narg++, (ushort) e_ins);
+		krnl_ksw_ext2.setArg(narg++, (ushort) w);
+		krnl_ksw_ext2.setArg(narg++, (ushort) h0);
+		krnl_ksw_ext2.setArg(narg++, buffer_m);
+
+		const auto initStartTimeH = std::chrono::high_resolution_clock::now();
+		//Launch the Kernel
+		q.enqueueTask(krnl_ksw_ext2);
+
+		// The result of the previous kernel execution will need to be retrieved in
+		// order to view the results. This call will write the data from the
+		// buffer_result cl_mem object to the source_results vector
+		q.enqueueMigrateMemObjects(outBufVec, CL_MIGRATE_MEM_OBJECT_HOST);
+		q.finish();
+		const auto initEndTimeH = std::chrono::high_resolution_clock::now();
+		const auto total_nsH = std::chrono::duration_cast<std::chrono::nanoseconds>(initEndTimeH - initStartTimeH).count();
+
+		struct ResultC RezC;
+		const auto initStartTime = std::chrono::high_resolution_clock::now();
+		RezC.max = -h0
+				+ ksw_extend2(qr_len, (uint8_t*) qr_c, db_len, (uint8_t*) db_c,
+						m, (int8_t*) simat, o_del, e_del, o_ins, e_ins, w,
+						end_bonus, 0, h0, &RezC.max_j, &RezC.max_i,
+						&RezC.max_ie, &RezC.gscore, &RezC.max_off);
+		printf("C++: max_j=%d max_i=%d max_ie=%d gscore=%d max_off=%d max=%d\n",
+				RezC.max_j, RezC.max_i, RezC.max_ie, RezC.gscore, RezC.max_off,
+				RezC.max);
+		const auto initEndTime = std::chrono::high_resolution_clock::now();
+		const auto total_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(initEndTime - initStartTime).count();
+		delete[] db_c;
+		delete[] qr_c;
+		delete[] simat;
+
+		printf("HLS: max_j=%d max_i=%d max_ie=%d gscore=%d max_off=%d max=%d\n",
+				source_m[0], source_m[1], source_m[2], source_m[3], source_m[4],
+				source_m[5] - h0);
+		std::cout << "C++ total_ns: " << total_ns << "\n";
+		std::cout << "HLS total_ns: " << total_nsH << "\n";
+		if ((RezC.max == (int) (source_m[5] - h0))
+				&& (RezC.max_j == (int) source_m[0])
+				&& (RezC.max_i == (int) source_m[1])
+				&& (RezC.max_ie == (int) source_m[2])
+				&& (RezC.gscore == (int) source_m[3])
+				&& (RezC.max_off == (int) source_m[4])) {
+			printf("test %d passed\n\n", test_j);
+		} else {
+			printf("test %d failed\n\n", test_j);
+			all_tests_passed = false;
+		}
+	}
+	if (all_tests_passed)
+		printf("All %d tests have passed\n", test_j - 1);
+	printf("_END_ \n\n");
+	return (all_tests_passed ? EXIT_FAILURE : EXIT_SUCCESS);
+}
---
+++ AWSF1_SDx_BWA_SW/src/ksw_ext2.cl
@@ -0,0 +1,202 @@
+// Configure the QR and DB max space on the FPGA
+#define QR_MAX 256
+#define DB_MAX 256
+// Configure size of the similarity matrix
+const ushort m = 5;
+
+typedef struct {
+	short h, e;
+} eh_t;
+
+// Similarity function implementation
+short similarity(ushort a, ushort b, ushort match, ushort mismatch,
+		ushort ambiguous) {
+#pragma HLS inline
+	if ((a == m - 1) || (b == m - 1)) {
+		return 0 - ambiguous;
+	} else if (a == b) {
+		return match;
+	} else
+		return 0 - mismatch;
+}
+
+kernel __attribute__((reqd_work_group_size(1, 1, 1)))
+// ksw_extend2_hls algorithm
+void ksw_ext2(const ushort qlen, global const uchar* source_q,
+		const ushort tlen, global const uchar* source_t, const ushort match,
+		const ushort mismatch, const ushort ambiguous, const ushort o_del,
+		const ushort e_del, const ushort o_ins, const ushort e_ins,
+		const ushort w, const ushort h0, global short* buffer_m) {
+	uchar query[QR_MAX];
+	uchar target[DB_MAX];
+
+	//printf("%d query: ", qlen);
+	readQ: for (int i = 0; i < qlen; i++) {
+		query[i] = source_q[i];
+		//printf("%d ", query[i]);
+	}
+	//printf("\n");
+	//printf("%d target: ", tlen);
+	readT: for (int i = 0; i < tlen; i++) {
+		target[i] = source_t[i];
+		//printf("%d ", target[i]);
+	}
+	//printf("\n");
+	//printf(
+	//		"match=%d mismatch=-%d ambiguous=-%d o_del=%d e_del=%d o_ins=%d e_ins=%d w=%d h0=%d\n",
+	//		match, mismatch, ambiguous, o_del, e_del, o_ins, e_ins, w, h0);
+
+	ushort i, oe_del = o_del + e_del, oe_ins = o_ins + e_ins;
+	short beg = 0, end = qlen, max = h0, max_i = -1, max_j = -1, max_ie = -1,
+			gscore = -1, max_off = 0, eh_h_prev;
+	bool break_i = false;
+
+	// VivadoHLS synthesizes arrays as BRAM
+	eh_t eh[QR_MAX + 1]; // score array
+
+	// fill the first row
+	eh[0].h = h0;
+	eh[0].e = 0; // 0
+
+	if (h0 > oe_ins) {
+		eh_h_prev = h0 - oe_ins;
+	} else {
+		eh_h_prev = 0;
+	}
+	eh[1].h = eh_h_prev;
+	eh[1].e = 0; // 1
+
+	LOOP_eh: for (i = 2; i <= qlen; ++i) {
+#pragma HLS dependence variable=eh inter false
+#pragma HLS PIPELINE
+		if (eh_h_prev > e_ins) {
+			eh_h_prev = eh_h_prev - e_ins;
+		} else {
+			eh_h_prev = 0;
+		}
+		eh[i].h = eh_h_prev;
+		eh[i].e = 0; // all other all the way to qlen
+	}
+
+	// DP loop
+	LOOP_i: for (i = 0; (i < tlen) && (break_i == false); ++i) {
+		short f = 0, h1, m = 0, mj = -1, j, abs_mji;
+
+		// apply the band and the constraint (if provided)
+		if (beg < i - w)
+			beg = i - w;
+		if (end > i + w + 1)
+			end = i + w + 1;
+		if (end > qlen)
+			end = qlen;
+
+		// compute the first column
+		if (beg == 0) {
+			h1 = h0 - (o_del + e_del * (i + 1));
+			if (h1 < 0)
+				h1 = 0;
+		} else
+			h1 = 0;
+
+		LOOP_j: for (j = beg; j < end; ++j) {
+#pragma HLS PIPELINE
+#pragma HLS dependence variable=eh inter false
+
+			// At the beginning of the loop: eh[j] = { H(i-1,j-1), E(i,j) }, f = F(i,j) and h1 = H(i,j-1)
+			// Similar to SSE2-SW, cells are computed in the following order:
+			//   H(i,j)   = max{H(i-1,j-1)+S(i,j), E(i,j), F(i,j)}
+			//   E(i+1,j) = max{H(i,j)-gapo, E(i,j)} - gape
+			//   F(i,j+1) = max{H(i,j)-gapo, F(i,j)} - gape
+			eh_t tmp = eh[j];
+			short h, t, q, M = tmp.h, e = tmp.e; // get H(i-1,j-1) and E(i-1,j)
+
+#pragma HLS dependence variable=tmp inter false
+#pragma HLS dependence variable=h inter false
+#pragma HLS dependence variable=t inter false
+#pragma HLS dependence variable=q inter false
+#pragma HLS dependence variable=M inter false
+#pragma HLS dependence variable=e inter false
+
+			tmp.h = h1;          // set H(i,j-1) for the next row
+
+			q = similarity(target[i], query[j], match, mismatch, ambiguous);
+			if (M) {
+				M = M + q;
+			} else {
+				M = 0;
+			} // separating H and M to disallow a cigar like "100M3I3D20M"
+
+			h = M > e ? M : e; // e and f are guaranteed to be non-negative, so h>=0 even if M<0
+			h = h > f ? h : f;
+			h1 = h;             // save H(i,j) to h1 for the next column
+			if (m <= h) {
+				mj = j;
+			} // record the position where max score is achieved
+			m = m > h ? m : h;   // m is stored at eh[mj+1]
+
+			if (M > oe_del) {
+				t = M - oe_del;
+			} else {
+				t = 0;
+			}
+			e -= e_del;
+			e = e > t ? e : t;   // computed E(i+1,j)
+			tmp.e = e;          // save E(i+1,j) for the next row
+			if (M > oe_ins) {
+				t = M - oe_ins;
+			} else {
+				t = 0;
+			}
+			f -= e_ins;
+			f = f > t ? f : t;   // computed F(i,j+1)
+			eh[j] = tmp;
+			//printf("%d %d eh%d ee%d M%d e%d f%d t%d m%d mj%d hp%d h%d b%d en%d \n",(int)i,(int)j,(int)eh[j].h,(int)eh[j].e,(int)M,(int)e,(int)f,(int)t,(int)m,(int)mj,(int)h1,(int)h,(int)beg,(int)end);
+
+		}
+		eh[end].h = h1;
+		eh[end].e = 0;
+
+		if (j == qlen) {
+			if (gscore <= h1) {
+				max_ie = i;
+				gscore = h1;
+			}
+		}
+
+		if (m == 0)
+			break_i = true;
+		if (m > max) {
+			max = m, max_i = i, max_j = mj;
+			abs_mji = (mj - i > 0) ? (mj - i) : (i - mj);
+			max_off = max_off > abs_mji ? max_off : abs_mji;
+		}
+
+		// update beg and end for the next round
+		LOOP_beg: for (j = beg; (j < end) && eh[j].h == 0 && eh[j].e == 0;
+				++j) {
+#pragma HLS dependence variable=eh inter false
+#pragma HLS PIPELINE
+		}
+		beg = j;
+
+		LOOP_end: for (j = end; (j >= beg) && eh[j].h == 0 && eh[j].e == 0;
+				--j) {
+#pragma HLS dependence variable=eh inter false
+#pragma HLS PIPELINE
+		}
+		if (j + 2 < qlen) {
+			end = j + 2;
+		} else {
+			end = qlen;
+		}
+		//beg = 0; end = qlen; // uncomment this line for debugging
+	}
+	buffer_m[0] = max_j + 1;
+	buffer_m[1] = max_i + 1;
+	buffer_m[2] = max_ie + 1;
+	buffer_m[3] = gscore;
+	buffer_m[4] = max_off;
+	buffer_m[5] = max;
+	//printf("%d %d %d %d %d %d \n", buffer_m[0], buffer_m[1], buffer_m[2],
+	//		buffer_m[3], buffer_m[4], buffer_m[5]);
+}
